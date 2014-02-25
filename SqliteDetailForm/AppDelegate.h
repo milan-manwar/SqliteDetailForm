c650033acd0e8cb269a79e89a2fae1e28797f4cc
Milan Manwar <milan.manwar@gmail.com>
2014-02-25 14:13:46 +0530

    Initial Commit

---
+++ SqliteDetailForm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SqliteDetailForm
+//
+//  Created by Omnipresent on 25/02/14.
+//  Copyright (c) 2014 iOSCuriosity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SqliteDetailForm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SqliteDetailForm
+//
+//  Created by Omnipresent on 25/02/14.
+//  Copyright (c) 2014 iOSCuriosity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
